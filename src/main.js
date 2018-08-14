dc9f26614f9ee25a6202667fbb7fa4258b8a6c36
wormslab <wormslab@gmail.com>
2018-08-14 19:20:32 +0900

    test: dynamic add column 테스트

--- src/main.js
+++ src/main.js
@@ -5,16 +5,20 @@ import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.css'
 import VuetifyTable from './VuetifyTable.vue'
 import TransitionOnEvent from './TransitionOnEvent'
+import DynamicAddColumn from './DynamicAddColumn'
 Vue.config.productionTip = false
 
 Vue.use(Vuetify);
 Vue.component('vuetify-table', VuetifyTable);
 Vue.component('transition-on-event', TransitionOnEvent);
+Vue.component('dynamic-add-column', DynamicAddColumn);
+
 
 new Vue({
   render (h) {
     return h('v-app', [
-      h('div', { class: 'elevation-1 ma-3' }, [ h('transition-on-event') ])
+      h('div', { class: 'elevation-1 ma-3' }, [ h('transition-on-event') ]),
+      h('div', { class: 'elevation-1 ma-3' }, [ h('dynamic-add-column') ])
 
     ])
   }
